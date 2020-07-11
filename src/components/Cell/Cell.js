0702f020d9a1b77b75b00c9bdd1776431cd6d875
John Lucker <john.lucker@comcast.net>
2020-07-11 09:45:05 -0400

    Added GameBoard
    Created GameBoard component which creates a board of 12 cells
    Created a Cell component which is used for each of the cells
    Updated Game component to call the GameBoard
    Updated CSS to make the GameBoard a visible 3x4 grid
    
    close #6

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,42 +1,42 @@
 import React, { useState, Fragment } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-function Cell({ id, handleChange, cell }) {
-    const [show, setShow] = useState(false)
+function Cell ({ id, handleChange, cell }) {
+  const [show, setShow] = useState(false)
 
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
-    const move = () => handleClose()
-    const attack = () => handleClose()
-    return (
-        <Fragment>
-            <p
-                id={id}
-                className={show ? `${sectionPosition} cell selected` : `${sectionPosition} cell`}
-                onClick={handleShow}
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const move = () => handleClose()
+  const attack = () => handleClose()
+  return (
+    <Fragment>
+      <p
+        id={id}
+        className={'cell'}
+        onClick={handleShow}
+      >
+        {cell}
+      </p>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Body>
+          <section>
+            <Button
+              variant="outline-dark"
+              onClick={() => { move() }}
             >
-                {cell}
-            </p>
-            <Modal show={show} onHide={handleClose} animation={false}>
-                <Modal.Body>
-                    <section>
-                        <Button
-                            variant="outline-dark"
-                            onClick={() => { move() }}
-                        >
-                            Move
-                        </Button>
-                        <Button
-                            variant="outline-dark"
-                            onClick={() => { attack() }}
-                        >
-                            Attack
-                        </Button>
-                    </section>
-                </Modal.Body>
-            </Modal>
-        </Fragment>
-    )
+              Move
+            </Button>
+            <Button
+              variant="outline-dark"
+              onClick={() => { attack() }}
+            >
+              Attack
+            </Button>
+          </section>
+        </Modal.Body>
+      </Modal>
+    </Fragment>
+  )
 }
 
 export default Cell
