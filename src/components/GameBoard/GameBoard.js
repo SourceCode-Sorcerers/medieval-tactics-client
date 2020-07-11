43a28d603865fc603b8205508e89b2d9fac9f463
John Lucker <luckyswims@gmail.com>
2020-07-11 09:13:10 -0400

    Working on GameBoard
    Started working on the GameBoard component.

---
+++ src/components/GameBoard/GameBoard.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+// import apiUrl from '../../apiConfig'
+// import axios from 'axios'
+import Cell from '../Cell/Cell'
+
+const GameBoard = ({ user, gameId, startingBoard, msgAlert }) => {
+    const [board, setBoard] = useState([])
+    useEffect(() => {
+        setBoard(startingBoard)
+    }, [startingBoard])
+    const handleChange = (cellId, value) => {
+        const newBoard = cleanBoard.map(value => value)
+        if (value) {
+        newBoard[cellId] = Number(value)
+        } else {
+        newBoard[cellId] = ''
+        }
+    }
+    const cleanBoard = [...board].map(cell => typeof cell === 'number' ? cell : '')
+    const boardJsx = cleanBoard.map((cell, index) => (
+        <Cell
+            key={index}
+            id={index}
+            handleChange={handleChange}
+            cell={cell}
+        />
+    ))
+    return (
+        <form className='board'>
+            {boardJsx}
+        </form>
+    )
+}
+
+export default GameBoard
