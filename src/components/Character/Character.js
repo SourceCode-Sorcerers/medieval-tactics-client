c72df04c59d7e97999fa219f5f7a00156c25ebaf
John Lucker <john.lucker@comcast.net>
2020-07-11 12:37:55 -0400

    Working on adding Character Component
    Created new character component
    Updated Cell to reference the character component

---
+++ src/components/Character/Character.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const Character = () => {
+  return (
+    <Fragment>
+      <img
+        src="https://i.imgur.com/MFlDp7d.png"
+      />
+    </Fragment>
+  )
+}
+
+export default Character
