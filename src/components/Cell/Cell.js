afae66179b2061fc993c27d0438c7a3e0ce1fb36
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-07-13 11:36:01 -0400

    Wrote player ownership and game started logic
    - Added player and gameStarted state variables to game.js
    - created setter function for player and gameStarted
    - added game over and game Start switch cases
    - added ternary to display appropriate messages to users

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react'
 import Character from '../Character/Character'
 
-function Cell ({ id, cell, move, dealDamage, board }) {
+function Cell ({ player, id, cell, move, dealDamage, board, gameStarted }) {
   return (
     <Fragment>
       <p
         id={id}
         className={'cell'}
       >
-        {(cell === 'player1' || cell === 'player2') ? <Character move = {move} dealDamage = {dealDamage} board = {board} cellId = {id} player = {cell}/> : cell}
+        {(cell === 'player1' || cell === 'player2') ? <Character move = {move} dealDamage = {dealDamage} player = {player} board = {board} cellId = {id} gameStarted = {gameStarted} owner = {cell}/> : cell}
       </p>
     </Fragment>
   )
--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,41 +1,47 @@
 import React, { useState, Fragment } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import img from '../../Resources/Character1.png'
+import img2 from '../../Resources/Character2.png'
 
-const Character = ({ board, move, dealDamage, cellId, player }) => {
+const Character = ({ player, board, move, dealDamage, cellId, owner, gameStarted }) => {
   const [show, setShow] = useState(false)
-  const [playerName] = useState(player)
+  const [ownerName] = useState(owner)
 
   const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const handleShow = () => {
+    console.log(gameStarted)
+    if (player === owner && gameStarted) {
+      setShow(true)
+    }
+  }
 
   const moveUp = () => {
     // Player position index -4
     // Cannot use if in index 3 or less || player2 is there
-    move(playerName, cellId, cellId - 4)
+    move(ownerName, cellId, cellId - 4)
     handleClose()
   }
   const moveRight = () => {
     // Player position index +1
     // Cannot use if in index 11 || player2 is there
-    move(playerName, cellId, cellId + 1)
+    move(ownerName, cellId, cellId + 1)
     handleClose()
   }
   const moveDown = () => {
     // Player position index +4
     // Cannot use if in index 8 or more || player2 is there
-    move(playerName, cellId, cellId + 4)
+    move(ownerName, cellId, cellId + 4)
     handleClose()
   }
   const moveLeft = () => {
     // Player position index -1
     // Cannot use if in index 0 || player2 is there
-    move(playerName, cellId, cellId - 1)
+    move(ownerName, cellId, cellId - 1)
     handleClose()
   }
   const attack = () => {
     let target
-    if (playerName === 'player1') {
+    if (ownerName === 'player1') {
       target = 'player2'
     } else {
       target = 'player1'
@@ -94,7 +100,7 @@ const Character = ({ board, move, dealDamage, cellId, player }) => {
   }
 
   let enemySpace
-  if (playerName === 'player1') {
+  if (ownerName === 'player1') {
     enemySpace = board.findIndex(name => name === 'player2')
   } else {
     enemySpace = board.findIndex(name => name === 'player1')
@@ -115,7 +121,7 @@ const Character = ({ board, move, dealDamage, cellId, player }) => {
     <Fragment>
       <img
         onClick={handleShow}
-        src={img}
+        src={ ownerName === 'player1' ? img2 : img }
       />
       <Modal show={show} onHide={handleClose} animation={true} centered={true}>
         <Modal.Body>
